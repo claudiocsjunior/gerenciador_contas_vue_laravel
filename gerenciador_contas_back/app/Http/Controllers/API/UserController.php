a168b598f542b4ee6656be9113f704a1c73f64a2
claudiocsjunior <claudio_junior15@hotmail.com>
2019-10-29 22:41:57 -0300

    Inicia o projeto front

--- gerenciador_contas_back/app/Http/Controllers/API/UserController.php
+++ gerenciador_contas_back/app/Http/Controllers/API/UserController.php
@@ -65,6 +65,6 @@ class UserController extends Controller
     public function details()
     {
         $user = Auth::user();
-        return response()->json(['success' => $user], $this-> successStatus);
+        return response()->json(['user  ' => $user], $this-> successStatus);
     }
 }
---
+++ gerenciador_contas_front/gerenciador_contas/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import './plugins/axios'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+//Imports adicionados
+import bootstrapVue from 'bootstrap-vue';
+import './assets/scss/app.scss'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  bootstrapVue,
+  render: h => h(App)
+}).$mount('#app')
